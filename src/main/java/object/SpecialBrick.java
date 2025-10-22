fb13dc26ade8d6a8696dd546b998dd3b1259bb28
24020272-lab <24020272@vnu.edu.vn>
2025-10-22 22:22:57 +0700

    version 2.1

--- src/main/java/object/SpecialBrick.java
+++ src/main/java/object/SpecialBrick.java
@@ -1,5 +1,20 @@
 //package object;
 //
-//public class SpecialBrick extends Brick {
+//import javafx.scene.canvas.GraphicsContext;
+//import javafx.scene.paint.Color;
+//import process.PlayingProcess;
 //
+//public class SpecialBrick extends NormalBrick {
+//    public SpecialBrick(double x, double y, double w, double h) {
+//        super(x, y, w, h, 1);
+//    }
+//
+//    @Override
+//    public void render(GraphicsContext gc) {
+//        gc.setFill(Color.RED);
+//        gc.fillRect(getX(), getY(), getWidth(), getHeight());
+//    }
+//
+//    @Override
+//    public void update(PlayingProcess gm){}
 //}
