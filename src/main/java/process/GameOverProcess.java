875508714f8217d0b61663e880d3816136efa3d8
24020272-lab <24020272@vnu.edu.vn>
2025-11-05 16:00:00 +0700

    FINAL VERSION

--- src/main/java/process/GameOverProcess.java
+++ src/main/java/process/GameOverProcess.java
@@ -1,69 +1,73 @@
 package process;
 
-
-import UI.OptionButton;
 import UI.QuitButton;
-import UI.StartButton;
+import UI.ReplayButton;
 import gamemanager.GameManager;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.TextAlignment;
-
 import javafx.stage.Stage;
 
-import java.util.Objects;
+import java.io.File;
 
 public class GameOverProcess extends Process {
-    private int score = 0;
+    private boolean uiInitialized = false;
+    private ReplayButton replayButton;
+    private QuitButton quitButton;
+    private int selectedButtonIndex = 0;
 
-  public GameOverProcess(int width, int height) {
-      super(width,height);
-  }
+    public GameOverProcess(int width, int height) {
+        super(width, height);
+        this.pane.getChildren().clear();
+    }
 
+    private void initUI(Stage stage, GameManager gameManager) {
+        String filePath = "file:src" + File.separator + "main" + File.separator + "resources"
+                + File.separator + "image" + File.separator + "GameOver.png";
+        Image bgImage = new Image(filePath);
+        ImageView bgView = new ImageView(bgImage);
+        bgView.setFitWidth(this.width);
+        bgView.setFitHeight(this.height);
+        this.pane.getChildren().add(0, bgView);
 
-  @Override
-  public void update(Stage stage, GameManager gm) {
-    this.scene.setOnKeyPressed(e -> {
-        if (Objects.requireNonNull(e.getCode()) == KeyCode.ESCAPE) {
-            System.exit(0);
+        int btnWidth = 250;
+        int btnHeight = 60;
+
+        replayButton = new ReplayButton("Replay", Pos.CENTER, new Insets(0, 0, 50, 0), btnWidth,
+                btnHeight, gameManager, stage);
+
+        quitButton = new QuitButton("Quit", Pos.CENTER, new Insets(50, 0, 0, 0), btnWidth, btnHeight);
+
+        this.pane.getChildren().addAll(replayButton, quitButton);
+
+        updateButtonFocus();
+    }
+
+    private void updateButtonFocus() {
+        if (selectedButtonIndex == 0) {
+            replayButton.requestFocus();
+        } else {
+            quitButton.requestFocus();
         }
-    });
-    if(!isAddButton()) {
-        addStartButton(stage, gm);
-        addQuitButton();
-        addButtonDone();
-    }
-  }
-
-   private void addStartButton(Stage stage, GameManager gameManager) {
-        pane.getChildren().add(
-            new StartButton("Start", Pos.BOTTOM_RIGHT, new Insets(0, 455, 260, 0),
-            250,60, gameManager,stage));
     }
 
-    private void addQuitButton() {
-        pane.getChildren().add(
-            new QuitButton("Quit", Pos.BOTTOM_RIGHT, new Insets(0, 455, 180, 0),
-            250,60
-            ));
+    @Override
+    public void update(Stage stage, GameManager gm) {
+        if (!uiInitialized) {
+            initUI(stage, gm);
+            uiInitialized = true;
+        }
+        this.scene.setOnKeyPressed(e -> {
+            KeyCode code = e.getCode();
+            if (code == KeyCode.UP || code == KeyCode.DOWN) {
+                selectedButtonIndex = 1 - selectedButtonIndex;
+                updateButtonFocus();
+            }
+        });
     }
 
-  @Override
-  public void render() {
-    gc.save();
-    gc.setFill(Color.BLACK);
-    gc.fillRect(0, 0, width, height);
-    gc.setFont(Font.font("Verdana", FontWeight.BOLD, 50));
-    gc.setFill(Color.RED);
-    gc.setTextAlign(TextAlignment.CENTER);
-    gc.fillText("GAME OVER", width / 2.0, height / 2.0);
-    gc.setFont(Font.font("Verdana", FontWeight.NORMAL, 20));
-    gc.setFill(Color.WHITE);
-    gc.fillText("Press SPACE to Play Again", width / 2.0, height / 2.0 + 40);
-    gc.restore();
-  }
+    @Override
+    public void render() {}
 }
